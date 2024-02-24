e9a75bc728fb3d4f1bdf998f3eadcd0172aace8e
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-24 11:39:16 +0530

    Navbar Add

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+import { Link, NavLink } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
 
 const Header = () => {
-  return (
-    <>
-        <nav className='bg-gray-500 px-8 py-3 flex justify-between'>
-            <div className="flex items-center gap-4">
-                <IoMenu className='text-3xl text-white cursor-pointer'/>
-                <div className="text-white">
-                    <h3>Logo</h3>
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    // const menus = [
+    //     {
+    //         label: "Home",
+    //         link: "/"
+    //     },
+    //     {
+    //         label: "About",
+    //         link: "/about"
+    //     },
+    //     {
+    //         label: "Services",
+    //         link: "/service"
+    //     },
+    //     {
+    //         label: "Pages",
+    //         link: "/pages"
+    //     },
+    //     {
+    //         label: "Blog",
+    //         link: "/blog"
+    //     },
+    //     {
+    //         label: "Contact",
+    //         link: "/contact"
+    //     },
+    // ];
+
+    return (
+        <>
+            <nav className='bg-[#1B3C73] px-8 py-3 flex justify-between relative'>
+                <div className="flex items-center gap-4">
+                    <IoMenu className='text-3xl text-white cursor-pointer lg:hidden' onClick={() => setSidebarOpen(true)} />
+                    <div className="text-white">
+                        <h3 className='text-2xl font-bold'>Dha<span className='text-[#F31559]'>ram</span></h3>
+                    </div>
+                    <div className="hidden lg:block">
+                        <ul className='flex gap-4 text-white'>
+                        {
+                            menus.map((lab, index)=>{
+                                return <li key={index} className='text-lg cursor-pointer hover:text-[#FF407D]'>
+                                    <NavLink to={lab.link}>{lab.label}</NavLink>
+                                </li>
+                            })
+                        }
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="">
-                <div className="w-12 h-12 cursor-pointer rounded-full overflow-hidden">
-                    <img src="https://avatars.githubusercontent.com/u/122605883?v=4" alt="user" />
+                <div>
+                    <div className="w-12 h-12 cursor-pointer rounded-full overflow-hidden">
+                        <img src="https://avatars.githubusercontent.com/u/122605883?v=4" alt="user" />
+                    </div>
                 </div>
-            </div>
+            </nav>
 
             {/* Sidebar Menu */}
-        </nav>
-    </>
-  )
-}
+            <div className={`fixed lg:hidden top-0 w-full h-full bg-[#0000004b] left-[-100% !important] ${sidebarOpen ? "left-[0]" : "left-[-100%]"}`}>
+                {/* <div className="w-[250px] h-full p-8 bg-white"> */}
+                <div className={`w-[250px] h-full p-8 bg-white transition-transform duration-300 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                    <IoMdClose className='text-2xl cursor-pointer mb-5' onClick={() => setSidebarOpen(false)} />
+                    <div>
+                        {menus.map((menu, index) => (
+                            <button key={index} className='block bg-[#6587b7] text-white mb-3 py-2 w-full rounded-xl hover:bg-[#40679E]'>{menu.label}</button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Layout from './Layout.jsx';
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx';
+import Blog from './Components/Blog/Blog.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path: "about",
+//         element: <About/>
+//       },
+//       {
+//         path: "blog",
+//         element: <Blog/>
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='blog' element={<Blog/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
